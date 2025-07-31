a3423953772e7c3b9d2473de27bf6f9e279213a3
Michael Holmes <michael@spatiosystems.com>
2025-07-31 17:01:22 -0600

    Implement v2: self-hosted server

--- code/express-discorder/server.js
+++ code/express-discorder/server.js
@@ -7,7 +7,8 @@ const port = 3000;
 
 app.use(express.json());
 
-app.get("/", (req, res) => res.send(`
+app.get("/", (req, res) =>
+	res.send(`
   <html>
     <head><title>Success!</title></head>
     <body>
@@ -15,36 +16,48 @@ app.get("/", (req, res) => res.send(`
       <img src="https://media.giphy.com/media/XreQmk7ETCak0/giphy.gif" alt="Cool kid doing thumbs up" />
     </body>
   </html>
-`));
+`)
+);
 
 app.post("/github", (req, res) => {
-  const content = ":wave: Hi mom!";
-  const avatarUrl = "https://media.giphy.com/media/3o7TKSjRrfIPjeiVyM/giphy.gif";
-  axios
-    .post(process.env.DISCORD_WEBHOOK_URL, {
-      content: content,
-      embeds: [
-        {
-          image: {
-            url: avatarUrl,
-          },
-        },
-      ],
-    })
-    .then((discordResponse) => {
-      console.log("Success!");
-      res.status(204).send();
-    })
-    .catch((err) => console.error(`Error sending to Discord: ${err}`));
+    // TODO: Chang ethe content variable to contain the repository name and
+    // the github user name...and emoji stuff.
+	let content = ":wave: Hi mom!";  // Text of the response
+
+	const avatarUrl = req.body.sender.avatar_url;
+    const userName = req.body.sender.login;
+    const repoName = req.body.repository.full_name;
+    const starred = req.body.action === "created"
+    const numRepoStars = req.body.repository.stargazers_count;
+
+    content += "\n" + `:person: ${userName} ${starred ? "starred" : "unstarred"} my repo ${repoName}.`
+    content += "\n" + `${repoName} now has ${numRepoStars} stars :star:!`
+
+	axios
+		.post(process.env.DISCORD_WEBHOOK_URL, {
+			content: content,
+			embeds: [
+				{
+					image: {
+						url: avatarUrl,
+					},
+				},
+			],
+		})
+		.then((discordResponse) => {
+			console.log("Success!");
+			res.status(204).send();
+		})
+		.catch((err) => console.error(`Error sending to Discord: ${err}`));
 });
 
 app.use((error, req, res, next) => {
-  res.status(500)
-  res.send({error: error})
-  console.error(error.stack)
-  next(error)
-})
+	res.status(500);
+	res.send({ error: error });
+	console.error(error.stack);
+	next(error);
+});
 
 app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
+	console.log(`Example app listening at http://localhost:${port}`)
 );
